ff80db7740eb273f8f5d16d0031a1534b093dd0f
BSUGD <tolik.senich.99@mail.ru>
2020-04-17 21:30:32 +0300

    9 with composer

--- Anatoly/AnatolyException.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Anatoly;
-class AnatolyException extends \RuntimeException {  // RuntimeException - класс ошибок, который есть сразу в php (со своими свойствами и метоадми, которые наследуются)
-}
-?>
--- build.php
+++ build.php
@@ -1,5 +1,26 @@
 <?php
+
+copy('https://getcomposer.org/installer', __DIR__ . '/composer-setup.php');
+if (hash_file('sha384', __DIR__ . '/composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a')
+{
+	echo 'Installer verified' . PHP_EOL;
+}
+else {
+	echo 'Installer corrupt' . PHP_EOL;
+	unlink(__DIR__ . '/composer-setup.php');
+}
+
+shell_exec(PHP_BINARY . ' ' . __DIR__ . '/composer-setup.php');
+unlink(__DIR__ . '/composer-setup.php');
+
+if(file_exists(__DIR__ . '/composer.phar')) {
+	shell_exec(PHP_BINARY . ' composer.phar install');
+	shell_exec(PHP_BINARY . ' autoload.php');
+}
+
+mkdir(__DIR__ . '/log');
 $version = trim(shell_exec('git symbolic-ref --short -q HEAD'));
-file_put_contents("version", $version);
-shell_exec('git clone https://725c98063a358e99b9478da41f5600e7a3ff4859@github.com/rok9ru/trpo-core.git core');
+file_put_contents(__DIR__ . "/version", $version);
+echo "Version " . $version . " is built";
+
 ?>
